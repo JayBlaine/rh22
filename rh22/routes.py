5696d2d4affb6cb03bf8a34ac3816e5bfa7ace31
Julie17XI <xi.tang717@gmail.com>
2022-03-26 16:04:01 -0500

    revise routes.py

--- rh22/routes.py
+++ rh22/routes.py
@@ -1,13 +1,17 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import render_template, url_for, flash, redirect, request, Flask, render_template
+from flask_login import login_user, current_user, logout_user, login_required
+from app import app, db, bcrypt
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from app.models import User
 from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+# def get_db_connection():
+#     conn = sqlite3.connect('database.db')
+#     conn.row_factory = sqlite3.Row
+#     return conn
 
 def get_account(account_id):
     # conn = get_db_connection()
@@ -23,32 +27,66 @@ def get_anime(anime_id):
 
 
 @app.route("/")
+@app.route("/home")
 def home():
-    # conn = get_db_connection()
-    # posts = conn.execute("SELECT * FROM posts").fetchall()
-    # global_recommendations =
-    #
-    # conn.close()
-    return render_template('home.html', global_recommendations=global_recommendations)
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    def register():
+        if current_user.is_authenticated:
+            return redirect(url_for('home'))
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+            user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+            db.session.add(user)
+            db.session.commit()
+            flash('Your account has been created! You\'re now able to login.', 'success')
+            return redirect(url_for('login'))
+        return render_template('register.html', title='Register', form=form)
 
-@app.route("/<int:anime_id>")
-def anime(anime_id):
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@app.route("/anime")
+def anime():
     anime_info = get_anime(anime_id)
     return render_template('anime.html', anime_info=anime_info)
-
-@app.route("/<int:account_id>")
-def account(account_id):
-    account_info = get_account(account_id)
-    return render_template('account.html', account_info=account_info)
