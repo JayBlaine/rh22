aa12b8bff361d1fd23d189afc8d864cdc37bcb0f
jayblaine <$h@d0wb18DE>
2022-03-26 12:10:52 -0500

    init

---
+++ rh22/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+rh22 = Flask(__name__)
+rh22.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+rh22.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(rh22)
+bcrypt = Bcrypt(rh22)
+login_manager = LoginManager(rh22)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from rh22 import routes
---
+++ run.py
@@ -0,0 +1,4 @@
+from rh22 import rh22
+
+if __name__ == '__main__':
+    rh22.run(debug=True)
